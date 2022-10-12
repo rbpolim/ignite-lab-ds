60bf239b5908be73cdc41b74045c52a716119970
rbpolim <rodrigobpolim@gmail.com>
2022-10-12 10:21:28 -0300

    :lipstick: feat: added DS (colors/fontsize) into tailwindcss config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      "gray-100": "#E1E1E6",
+      "gray-200": "#C4C4CC",
+      "gray-400": "#7C7C8A",
+      "gray-800": "#202024",
+      "gray-900": "#121214",
+
+      "cyan-300": "#9BE1FB",
+      "cyan-500": "#81D8F7",
+    },
   },
   plugins: [],
 };
