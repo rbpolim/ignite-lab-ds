a07d40f12240e63057fa688d1f45a4f1452ef6de
rbpolim <rodrigobpolim@gmail.com>
2022-10-13 18:23:59 -0300

    :sparkles: enhancement: improved Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,22 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { clsx } from 'clsx'
 import { Slot } from '@radix-ui/react-slot'
 
-export type ButtonProps = {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
   const Component = asChild ? Slot : 'button'
 
   return (
     <Component
       className={clsx(
-        'text-black py-4 px-3 rounded text-sm w-full bg-cyan-500 font-semibold font-sans hover:bg-cyan-300 transition-colors focus:ring-2 ring-white',
+        'text-black py-3 px-4 rounded text-sm w-full bg-cyan-500 font-semibold font-sans hover:bg-cyan-300 transition-colors focus:ring-2 ring-white',
+        className
       )}
+      {...props}
     >
       {children}
     </Component>
