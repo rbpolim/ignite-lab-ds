9f59981be7ea5a5965c94b3567cc980fdcbbb7e2
rbpolim <rodrigobpolim@gmail.com>
2022-10-13 18:24:15 -0300

    :sparkles: enhancement: improved Checkbox component

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,9 +1,14 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
-export function Checkbox() {
+interface CheckboxProps extends CheckboxPrimitive.CheckboxProps { }
+
+export function Checkbox({ ...props }: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root className='h-6 w-6 p-[2px] bg-gray-800 rounded'>
+    <CheckboxPrimitive.Root
+      className='h-6 w-6 p-[2px] bg-black rounded'
+      {...props}
+    >
       <CheckboxPrimitive.Indicator>
         <Check weight='bold' className='h-5 w-5 text-cyan-500' />
       </CheckboxPrimitive.Indicator>
